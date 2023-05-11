7c6c13d326ce1e6579a2461e1ca896720a98cda6
ramiro <ceinnova13458@innovaccion.mx>
2023-05-10 23:36:03 -0600

    Se creó un object literal que almacena los valores de los selects del HTML (la moneda y la criptomoneda). Se agregó el formulario, así como los Select para agregarles un evento. submitFormulario() hace una validación para identificar los campos vacíos. leerValor() almacena el valor seleccionado de los <select> para añadirlo al atributo correspondiente en objBusqueda

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+const objBusqueda = {
+  moneda: '',
+  criptomoneda: '',
+};
 
 // Crear un Promise
 const obtenerCriptomonedas = (criptomonedas) =>
@@ -8,6 +14,11 @@ const obtenerCriptomonedas = (criptomonedas) =>
 
 document.addEventListener('DOMContentLoaded', () => {
   consultarCriptomonedas();
+
+  formulario.addEventListener('submit', submitFormulario);
+
+  criptomonedasSelect.addEventListener('change', leerValor);
+  monedaSelect.addEventListener('change', leerValor);
 });
 
 function consultarCriptomonedas() {
@@ -28,4 +39,24 @@ function selectCriptomonedas(criptomonedas) {
     option.textContent = FullName;
     criptomonedasSelect.appendChild(option);
   });
+}
+
+function leerValor(e) {
+  objBusqueda[e.target.name] = e.target.value;
+}
+
+function submitFormulario(e) {
+  e.preventDefault();
+
+  // Validar
+  const { moneda, criptomoneda } = objBusqueda;
+
+  if (moneda === '' || criptomoneda === '') {
+    mostrarAlerta('Ambos campos son obligatorios');
+    return;
+  }
+}
+
+function mostrarAlerta(msg) {
+  console.log(msg);
 }
